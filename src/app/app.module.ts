6ad1c59ad44e26ffc870bcbce049efc5efb05dcf
samosaaa <samra_adl@outlook.fr>
2023-10-06 14:38:22 +0200

    page accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ArticleComponent } from './article/article.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionComponent,
+    ConnexionComponent,
+    AccueilComponent,
+    ArticleComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
