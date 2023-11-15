36a7c7c643ba255428c4c96a81d67a23971650db
Lilia Bouroubi <lilia.bouroubi@gmail.com>
2023-11-15 22:15:19 +0100

    final cleaning

--- src/app/mock/app.mock.ts
+++ src/app/mock/app.mock.ts
@@ -1,16 +1,17 @@
 import {Server} from 'miragejs';
-import {productModels, users,reviews} from './data';
+import {productModels, users, reviews} from './data';
 import {ProductModel} from "../models/product";
 
 export default (): void => {
 
     new Server({
         seeds(server) {
-            server.db.loadData({productModels, users,reviews});
+            server.db.loadData({productModels, users, reviews});
         },
 
         routes() {
             this.namespace = 'api';
+            // Product CRUD \\
             // Route pour obtenir la liste des produits
             this.get('/products', (schema => schema.db['productModels']));
 
@@ -19,39 +20,6 @@ export default (): void => {
                 const productId = request.params['id'];
                 const test: ProductModel = schema.db['productModels'].findBy({id: productId});
                 return test;
-            });
-
-            // Route pour créer un nouveau produit (si nécessaire)
-            this.post('/products', (schema, request) => {
-                const attrs = JSON.parse(request.requestBody);
-                return schema.create('product', attrs);
-            });
-
-            // Route pour mettre à jour un produit (si nécessaire)
-            this.put('/products/:id', (schema, request) => {
-                const productId = request.params['id'];
-                const attrs = JSON.parse(request.requestBody);
-                const product = schema.find('product', productId);
-                if (product) {
-                    product.update(attrs);
-                    return product;
-                } else {
-                    // Gérer le cas où le produit est introuvable
-                    return {error: 'Product not found'};
-                }
-            });
-
-            // Route pour supprimer un produit (si nécessaire)
-            this.delete('/products/:id', (schema, request) => {
-                const productId = request.params['id'];
-                const product = schema.find('product', productId);
-
-                if (product) {
-                    product.destroy();
-                    return null; // Renvoyer une réponse sans contenu
-                } else {
-                    return {error: 'Product not found'};
-                }
             });
 
 
@@ -65,7 +33,7 @@ export default (): void => {
                 try {
                     const newUser = JSON.parse(request.requestBody);
 
-                    // Vérifiez si l'utilisateur existe déjà
+                    // Pour vérifier si l'utilisateur existe déjà
                     const existingUser = users.find(user => user.mail === newUser.mail);
 
                     if (existingUser) {
@@ -97,95 +65,8 @@ export default (): void => {
                 }
             });
 
-
-            this.post('/admin/products', (schema, request) => {
-                const attrs = JSON.parse(request.requestBody);
-                const product = schema.create('product', attrs);
-                return product;
-            });
-
-            this.put('/admin/products/:id', (schema, request) => {
-                const productId = request.params['id'];
-                const attrs = JSON.parse(request.requestBody);
-                const product = schema.db['productModels'].findBy({id: productId});
-                if (product) {
-                    product.update(attrs);
-                    return product;
-                } else {
-                    return {error: 'Product not found'};
-                }
-            });
-
-            this.delete('/admin/products/:id', (schema, request) => {
-                const productId = request.params['id'];
-                const product = schema.db['productModels'].findBy({id: productId});
-                if (product) {
-                    product.destroy();
-                    return null; // Renvoyer une réponse sans contenu
-                } else {
-                    return {error: 'Product not found'};
-                }
-            });
-
-            // Ajoutez une route pour l'incrémentation du stock
-            this.put('/products/increment/:id', (schema, request) => {
-                const productId = request.params['id'];
-                const product = schema.db['productModels'].findBy({id: productId});
-
-                if (product) {
-                    product.stock += 1;
-                    return product;
-                } else {
-                    return {error: 'Product not found'};
-                }
-            });
-
-
-            // Ajoutez une route pour la décrémentation du stock
-            this.put('/products/decrement/:id', (schema, request) => {
-                const productId = request.params['id'];
-                const product = schema.db['productModels'].findBy({id: productId});
-
-                if (product && product.stock > 0) {
-                    product.stock -= 1;
-                    return product;
-                } else {
-                    return {error: 'Product not found or stock is already at zero'};
-                }
-            });
-
-            //Route pour commentaires
-            this.get('/reviews',schema=>schema.db['reviews'])
-            this.get('/reviews/:id',(schema,request)=>{
-                const productId:string = request.params['id'];
-                const productFind: ProductModel = schema.db['productModels'].findBy({id: productId});
-                return schema.db['reviews'].where({productId:productFind.id})
-            });
-
-            this.get('/reviews/user/:firstName',(schema,request)=>{
-                const userFind:string = request.params['firstName'];
-                return schema.db['reviews'].where({user:userFind})
-            });
-
-            this.post('/reviews',(schema,request)=>{
-                const review:string = JSON.parse(request.requestBody);
-                return schema.db['reviews'].insert(review);
-            });
-
-            this.put('/reviews/:idReviews',(schema,request)=>{
-                const reviewId:string = request.params['id'];
-                const newReview = JSON.parse(request.requestBody);
-                const reviewFind = schema.db['reviews'].findBy({id: reviewId});
-                if (reviewFind) {
-                    // Mettez à jour les propriétés de l'avis avec les nouvelles valeurs
-                    reviewFind.update(newReview);
-                    return reviewFind;
-                } else {
-                    return { error: 'Review not found' };
-                }
-
-            });
-
+            //Route pour récupérer commentaires
+            this.get('/reviews', schema => schema.db['reviews'])
         }
     })
 }
--- src/app/services/reviews/reviews.service.ts
+++ src/app/services/reviews/reviews.service.ts
@@ -1,35 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {ReviewModel} from "../../models/review";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ReviewsService {
-  private baseUrl = '/api/reviews';
+    private baseUrl:string = '/api/reviews';
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {
+    }
 
-  public getReviews(): Observable<ReviewModel[]> {
-    const url = `${this.baseUrl}`;
-    return this.http.get<ReviewModel[]>(url);
-  }
+    public getReviews(): Observable<ReviewModel[]> {
+        const url: string = `${this.baseUrl}`;
+        return this.http.get<ReviewModel[]>(url);
+    }
 
-  public addReview(productId: string, newReview: ReviewModel): Observable<ReviewModel> {
-    const jsonBody:string =JSON.stringify(newReview);
-    const url = `${this.baseUrl}/${productId}`;
-    return this.http.post<ReviewModel>(url, jsonBody);
-  }
-
-  public updateReview(idReview:number,newReview:string):Observable<ReviewModel>{
-    const jsonBody:string =JSON.stringify(newReview);
-    const url = `${this.baseUrl}/${idReview}`;
-    return this.http.put<ReviewModel>(url,jsonBody);
-  }
-
-  public deleateReview(idReview:number):Observable<ReviewModel>{
-    const url = `${this.baseUrl}/${idReview}`;
-    return this.http.delete<ReviewModel>(url);
-  }
 }
